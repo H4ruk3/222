21474bd43496ae82095a3b99117988e01aa84173
H4ruk3 <146736090+H4ruk3@users.noreply.github.com>
2026-05-07 15:17:46 +0300

    Add files via upload

---
+++ Auth.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+
+
+class MainController extends AppController
+{
+
+	public function login() {
+
+
+	}
+
+	public function logout() {
+
+
+	}
+
+	public function reg() {
+
+		
+	}
+
+}
